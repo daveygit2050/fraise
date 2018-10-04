8a7310e843ea1b8541508d22a2235c6e28e42366
Dave Randall <dave@goldsquare.co.uk>
2018-10-04 15:05:33 +0100

    FR-19 Fixing error with importing in python3

--- fraise/common.py
+++ fraise/common.py
@@ -1,9 +1,9 @@
-import words
+from .words import get_random_word
 
 def generate(word_count = 4):
     word_list = []
     for _ in range(word_count):
-        word_list.append(words.get_random_word())
+        word_list.append(get_random_word())
     passphrase = ' '.join(map(str, word_list))
     return(passphrase)
 
