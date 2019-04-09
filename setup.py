352ff64099c005c0cdb0171cbc15abfa09ea3c44
Dave Randall <19395688+daveygit2050@users.noreply.github.com>
2019-04-09 11:00:49 +0100

    FR-17 Render markdown in PyPi

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='fraise',
-      version='1.4.1',
+      version='1.4.2',
       description='Generate memorable pass phrases',
       long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+      long_description_content_type="text/markdown",
       url='http://github.com/daveygit2050/fraise',
       author='Dave Randall',
       author_email='dave@goldsquare.co.uk',
